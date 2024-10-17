f383e14119c0d5ba3e7e2f499dcb09a40722efcf
AkshithGandhamASU <agandha1@asu.edu>
2024-10-17 14:43:30 -0700

    Made start on proj
    Co-authored-by: echen451 echen451@users.noreply.github.com Co-authored-by: Leo Bryant Leo-Bryant@users.noreply.github.com Co-authored-by: anmyers9 anmyers9@users.noreply.github.com Co-authored-by: pwidth pwidth@users.noreply.github.com

--- sketch.js
+++ sketch.js
@@ -1,12 +1,25 @@
 let colorlist = ['gold', 'yellow', 'turquoise', 'red']
 
+let buttons;
+let background_img;
+let game_title_img;
+let font;
+const windowWidth = 500, windowHeight = 500;
+
+function preload() {
+    game_title_img = loadImage("")
+    font = loadFont("MinecraftRegular-Bmg3.otf");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  buttons = [];
+  background(0);
 }
 
 function draw() {
   noStroke()
   fill(random(colorlist));
   ellipse(mouseX, mouseY, 25, 25);
+  buttons.display();
 }
